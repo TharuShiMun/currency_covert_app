29b459c7bcaf31eb75c0381060a84cac1c673cb9
TharuShiMun <139700921+TharuShiMun@users.noreply.github.com>
2024-12-29 10:04:49 +0000

    Initialize variables

--- myapp/App.js
+++ myapp/App.js
@@ -1,3 +1,16 @@
-import React, {useState} from 'react';
-import {View,Text} from 'react-native';
+import React, { useState } from 'react';
+import { View, Text } from 'react-native';
 
+export default function App() {
+ const [lkrAmount, setLkrAmount]=useState('');
+ const [usdAmount, setUsdAmount]=useState(null);
+
+ const convertRate=0.003;
+
+  
+
+  return (
+    <>
+    </>
+  );
+}

92e279cf8e25512d3b75acdffcdaa17d35e97a5d
TharuShiMun <139700921+TharuShiMun@users.noreply.github.com>
2024-12-29 10:25:28 +0000

    Add styles

--- myapp/App.js
+++ myapp/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Alert,StyleSheet } from 'react-native';
+import { Button, TextInput } from 'react-native-web';
 
 export default function App() {
  const [lkrAmount, setLkrAmount]=useState('');
@@ -7,10 +8,67 @@ export default function App() {
 
  const convertRate=0.003;
 
-  
+ const converSion = ()=>{
+  const amount = parseFloat(lkrAmount);
+  if (isNaN(amount) || amount<=0){
+    Alert.alert('Invalid Input','Please enter a valid amount in LKR');
+    return;
+  }
+  const convertedAmount =(amount * convertRate).toFixed(2);
+  setUsdAmount(convertedAmount);
+ }
 
   return (
-    <>
-    </>
+    <View>
+      <Text>LKR to USD Converter</Text>
+      <TextInput
+
+      placeholder='Enter amount in LKR'
+      keyboardType='numeric'
+      value={lkrAmount}
+      onChangeText={setLkrAmount}
+      
+      />
+
+      <Button title="Convert" onPress={converSion}/>
+      {usdAmount && (
+        <Text style={styles.result}>USD: ${usdAmount}</Text>
+      )}
+
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFFFE0',
+    padding: 16,
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: '#00008b', 
+    textDecorationLine: 'underline', 
+  },
+  input: {
+    height: 40,
+    width: '80%',
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 20,
+    backgroundColor: '#fff',
+    fontWeight: 'bold',
+  },
+  result: {
+    marginTop: 20,
+    fontSize: 18,
+    color: '#00008b', 
+    fontWeight: 'bold',
+  },
+});
